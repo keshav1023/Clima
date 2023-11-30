2fd5c10ffd6df7d9837539da7234c82f90f6a085
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-30 19:09:24 +0530

    custom weather info updated

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -15,6 +15,7 @@ class _LocationScreenState extends State<LocationScreen> {
   int? temperature;
   String? weatherIcon;
   String? cityName;
+  String? weatherMessage;
 
   void initState() {
     // updateUI(widget.locationWeather);
@@ -23,14 +24,14 @@ class _LocationScreenState extends State<LocationScreen> {
   }
 
   void updateUI(dynamic weatherData) {
-    double temp = weatherData['main']['temp'];
-    temperature = temp.toInt();
-    var condition = weatherData['weather'][0]['id'];
-    weatherIcon = weather.getWeatherIcon(condition!);
-
-    cityName = weatherData['name'];
-
-    print(temperature);
+    setState(() {
+      double temp = weatherData['main']['temp'];
+      temperature = temp.toInt();
+      var condition = weatherData['weather'][0]['id'];
+      weatherIcon = weather.getWeatherIcon(condition!);
+      weatherMessage = weather.getMessage(temperature!);
+      cityName = weatherData['name'];
+    });
   }
 
   @override
@@ -88,7 +89,7 @@ class _LocationScreenState extends State<LocationScreen> {
               Padding(
                 padding: EdgeInsets.only(right: 15.0),
                 child: Text(
-                  "It's 🍦 time in San Francisco!",
+                  '$weatherMessage in $cityName',
                   textAlign: TextAlign.right,
                   style: kMessageTextStyle,
                 ),
